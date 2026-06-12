71e222d6e6f72154b9501673626a65dbdfd17946
swaroopjaipur <swaroopjaipur175@gmail.com>
2026-06-13 03:33:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,13 +17,13 @@ else:
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "31329774"))
+API_ID = int(os.environ.get("API_ID", ""))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "6a69b6df9a74b5902b0b031bacb353b7")
+API_HASH = os.environ.get("API_HASH", "")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "7141848479"))
+ADMINS = int(os.environ.get("ADMINS", ""))
 
 # Your Channel Id In Which Bot Upload Downloaded Video/File/Message etc.
 # And Make Your Bot Admin In this channel with full rights.
